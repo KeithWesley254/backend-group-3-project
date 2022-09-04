84c5eecbceacc86bb495a05ff95ced0dfbbf7c98
+ <keithwesley187@gmail.com>
2022-09-04 12:03:47 +0300

    milestone: testing

--- db/migrate/20220903091628_create_students_table.rb
+++ db/migrate/20220903091628_create_students_table.rb
@@ -1,4 +1,4 @@
-class CreateStudentsTable < ActiveRecord::Migration[7.0]
+class CreateStudentsTable < ActiveRecord::Migration[6.1]
   def change
     create_table :students do |t|
       t.string :name
